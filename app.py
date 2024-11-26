10b8aa323dcd837e91a6f96a3d2cece554d2cae7
Sanjeev Halder <sanjeevhalder@Sanjeevs-MacBook-Air.local>
2024-11-27 02:27:17 +0530

    character counting

--- app.py
+++ app.py
@@ -4,22 +4,21 @@ import requests, re
 app = Flask(__name__)
 
 @app.route('/')
-def ticket_fields():
+def ticket_fields1():
     return render_template('line_converter.html')
 
-@app.route('/character_counting')
-def character_counting():
-    return render_template('character_counting.html')
-
 @app.route('/api', methods=["post"])
-def ticket_fields1():
+def ticket_fields2():
     text = str(request.get_json()["text"])
     text = ' '.join(text.split('\n'))
     return text
 
+@app.route('/character_counting')
+def character_counting1():
+    return render_template('character_counting.html')
 
 @app.route('/character_counting', methods=["post"])
-def character_counting():
+def character_counting2():
     text = str(request.get_json()["text"])
     text = len(text.strip())
     return str(text)
